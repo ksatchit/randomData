09c0198122912063de81b81219ce40f252a9d721
ksatchit <karthik.s@openebs.io>
2019-09-13 02:56:07 +0000

    metrics monitor
    Signed-off-by: ksatchit <karthik.s@openebs.io>

---
+++ kube-monitor/dockerfiles/chaos_collector.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+METRICS_GIT_URL="https://gitlab.com/litmuschaos/demo-app.git"
+METRICS_GIT_BRANCH="exporter"
+POLLING_INTERVAL="5"
+
+echo "METRICS REPO: ${METRICS_GIT_URL}"
+echo "METRICS BRANCH: ${METRICS_GIT_BRANCH}" 
+
+git init 
+git remote add origin ${METRICS_GIT_URL}
+
+echo "METRICS PULL INTERVAL: ${POLLING_INTERVAL}"
+
+while true
+do
+  echo -e "\nPulling metrics @ $(date)"
+  echo "************************************************"
+  git pull origin ${METRICS_GIT_BRANCH}  
+  echo "************************************************"
+  sleep ${POLLING_INTERVAL}
+done 
